9a7f4b26dede849efb1bab38e93d664dd893d96f
PierceCappa <cappa.pierce@gmail.com>
2023-04-29 20:26:06 -0700

    started testing

--- Source/Vector.c
+++ Source/Vector.c
@@ -11,30 +11,72 @@ struct Vector createVector(unsigned long long size, struct Input* starterValues)
 
 void push_back(struct Vector* vector, struct Input newValue)
 {
-    push_multiple(vector, &newValue, 1, vector->size - 1);
+    struct Input* newVectorValues = malloc(sizeof(struct Input) * (vector->size + 1));
+    if(vector->size != 0)
+    {
+        memcpy(newVectorValues, vector->values, sizeof(struct Input) * (vector->size));
+        free(vector->values);
+    }
+    vector->values = newVectorValues;
+    
+    vector->values[vector->size] = newValue;
+    vector->size = vector->size + 1;
 }
 
 void push(struct Vector* vector, struct Input newValue, unsigned long long index)
 {
-    push_multiple(vector, &newValue, 1, index);
+    struct Input* newVectorValues = malloc(sizeof(struct Input) * (vector->size + 1));
+    if(vector->size != 0)
+    {
+        unsigned long long above = vector->size - index + 1;
+        unsigned long long below = vector->size - index - 1;
+        if(above < vector->size)
+        {
+            memcpy(newVectorValues + above, vector->values + above, sizeof(struct Input) * (vector->size - above));
+        }
+        if(below > -1)
+        {
+            memcpy(newVectorValues + below, vector->values + below, sizeof(struct Input) * (below));
+        }
+        free(vector->values);
+    }
+    vector->values = newVectorValues;
+    
+    vector->values[index] = newValue;
+    vector->size = vector->size + 1;
 }
 
 void push_multiple(struct Vector* vector, struct Input* newValues, unsigned long long size, unsigned long long startingIndex)
 {
+    if(startingIndex > vector->size || startingIndex < 0 )
+    {
+        printf("error %d\n", startingIndex);
+        return;
+    }
+
     unsigned long long newVectorSize = sizeof(struct Input) * (vector->size + size);
     unsigned long long oldVectorSize = sizeof(struct Input) * (vector->size);
     struct Input* newVectorValues = malloc(newVectorSize);
 
-    if(!newValues && newVectorSize)
+    if(vector->size != 0)
     {
-        //throw error
-        printf("error allocating new values\n");
+        unsigned long long above = vector->size - startingIndex + size + 1;
+        unsigned long long below = vector->size - startingIndex - 1;
+        if(above < vector->size)
+        {
+            memcpy(newVectorValues + above, vector->values + above, sizeof(struct Input) * (vector->size - above));
+        }
+        if(below > -1)
+        {
+            memcpy(newVectorValues + below, vector->values + below, sizeof(struct Input) * (below));
+        }
+        free(vector->values);
     }
-
-    memcpy(newVectorValues, vector, oldVectorSize);
-    free(vector->values);
-    memcpy(&(newValues[startingIndex]), newValues, newVectorSize-oldVectorSize);
+    memcpy(newValues + startingIndex, newValues, sizeof(struct Input) * size);
     vector->values = newValues;
+    vector->size = vector->size + size;
+
+
 
 }
 
--- Source/main.c
+++ Source/main.c
@@ -3,7 +3,73 @@
 
 int main(){
     printf("Hello World\n");
+    struct Vector vec;
+    vec.size = 0;
+    vec.values = NULL;
 
+    int sampleInputSize = 100000;
+    /*
+
+    printf("===========Testing push_back===========\n");
+
+    for(int i = 0; i < sampleInputSize; i++)
+    {
+        struct Input sample;
+        sample.value = i;
+        push_back(&vec, sample);
+    }
+
+    printf("testing push_back");
+
+    if(vec.values[5000].value != 5000)
+    {
+        printf("Expected a value of 5000 at index 5000, got %d\n", vec.values[5000].value);
+    }
+    else
+    {
+        printf("test passed\n");
+    } */
+
+/*
+    printf("\n\n===========Testing push===========\n");
+    for(int i = 0; i < sampleInputSize; i++)
+    {
+        struct Input sample;
+        sample.value = i;
+        push(&vec, sample, 0);
+    }
+
+    printf("done testing push\n");
+
+
+    if(vec.values[sampleInputSize - 1].value != 0)
+    {
+        printf("Expected a value of 0 at index %d, got %d\n", sampleInputSize - 1, vec.values[sampleInputSize - 1].value);
+    }
+    else
+    {
+        printf("test passed\n");
+    }*/
+
+    printf("\n\n===========Testing push_multiple===========\n");
+    for(int i = 0; i < sampleInputSize/10; i++)
+    {
+        struct Input sample[10];
+        sample[0].value = i;
+        push_multiple(&vec, sample, 10, 0);
+    }
+
+    printf("done testing push_multiple\n");
+
+
+    if(vec.values[10 * 10].value != 10)
+    {
+        printf("Expected a value of 10 at index %d, got %d\n", 10 * 10, vec.values[10 * 10].value);
+    }
+    else
+    {
+        printf("test passed\n");
+    }
 
 
     return 0;
